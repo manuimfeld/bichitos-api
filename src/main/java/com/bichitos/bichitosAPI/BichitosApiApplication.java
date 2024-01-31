c6ab5e8c9ae48e4e06bc94a811b04c2524c3252a
manuimfeld <manu_jadir@hotmail.com>
2024-01-31 16:52:22 -0300

    first commit

---
+++ src/main/java/com/bichitos/bichitosAPI/BichitosApiApplication.java
@@ -0,0 +1,13 @@
+package com.bichitos.bichitosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BichitosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BichitosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bichitos/bichitosAPI/BichitosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bichitos.bichitosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BichitosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
